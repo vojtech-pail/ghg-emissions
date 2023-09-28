25cda940bc9a4524a38361aed6962d6e7b81c0d5
Vojtech Pail <pail.vojtech@gmail.com>
2023-09-28 18:25:30 +0000

    Replacing divisions with SAFE_DIVIDE() function.

--- dbt/models/core/datasets_comparison_granular.sql
+++ dbt/models/core/datasets_comparison_granular.sql
@@ -3,7 +3,7 @@ with totals_comparison as (
     select
         *,
         cw_value - fao_value as values_difference, -- real difference than reported (Mt CO2)
-        round((cw_value - fao_value) / fao_value, 4) as relative_difference -- real difference than reported (%)
+        round(SAFE_DIVIDE((cw_value - fao_value), fao_value), 4) as relative_difference -- real difference than reported (%)
 
     from
         {{ ref('stg_datasets_comparison_granular') }}
